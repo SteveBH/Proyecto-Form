94f71d759c52ece3bd5a8acf79a983b01e291a76
Bryan Heredia <bshpulido@gmail.com>
2023-06-02 08:04:22 -0500

    First form

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Button, Badge } from "react-bootstrap";
+import NavBarMenu from "./components/NavBarMenu";
+import Introduction from "./components/Introduction";
+import Services from "./components/Services/Services";
+import DynamicForm from "./components/DynamicForm/DynamicForm";
+
+
+function App() {
+  return (
+    <div>
+      <NavBarMenu />
+      <Introduction />
+      <Services />
+      <DynamicForm />
+    </div>
+  );
+}
+
+export default App;
